fedb7c563b230da9dc4643b31a7b8e18d17e6e72
rahul <rahulparmar7654321@gmail.com>
2022-03-05 10:15:31 +0530

    aa

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -44,9 +44,9 @@ const index = () => {
             <h2>My Recent Projects</h2>
             <div className="projects">
               <ProjectCard 
-                title="Travel_duniya" 
-                subtitle="Created a full stack application as an online travel advisor which helps you to find restaurants, hotels and attraction on your current location, its fast and user friendly " 
-                techlist={['React Js','Js',"Material UI","Google map api"]}
+                title="Edtech Saas Platform" 
+                subtitle="Worked on a EdTech Startup Neurolingua ,building end-to-end solutions " 
+                techlist={['React Js',"Redux","axios",'Styled Components',"BootStrap","api"]}
                 gitlink={"https://github.com/RAHUL-6611/neurolingua-development"}
                 deploylink={"https://neurolingua-rahul.netlify.app/"}
                 deploy
